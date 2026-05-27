f9e82f271a799809f98e7ed87d30c7fec5cab4ae
yigabriel <gabrielsacc7@gmail.com>
2026-05-26 21:55:49 -0500

    feat: add supabase bd

--- src/main/java/com/upc/pe/managementcafelab/ManagementCafeLabApplication.java
+++ src/main/java/com/upc/pe/managementcafelab/ManagementCafeLabApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ManagementCafeLabApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ManagementCafeLabApplication.class, args);
     }
-
 }
